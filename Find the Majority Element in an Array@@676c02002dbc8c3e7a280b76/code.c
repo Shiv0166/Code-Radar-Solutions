95516c407697965698d9764e77e58a1edd81eaf9
Shiv0166 <shivamrajpoot0616@gmail.com>
2025-04-09 22:48:14 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -7,38 +7,48 @@ int main() {
     int visited = -1;
     int arr[n], freq[n];
 
+    // Input and initialize
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
-        freq[i] = 0;  
+        freq[i] = 0;
     }
 
-    // Frequency counting
+    // Count frequency
     for(int i = 0; i < n; i++) {
-        if(freq[i] == visited) {
-            continue; 
-        }
+        if(freq[i] == visited)
+            continue;
 
         int count = 1;
         for(int j = i + 1; j < n; j++) {
             if(arr[i] == arr[j]) {
                 count++;
-                freq[j] = visited; 
+                freq[j] = visited;
             }
         }
-
         freq[i] = count;
     }
-    int maxfreq = -1;
-    int maxelement = -1;
 
+    // Find max frequency and count how many elements have it
+    int maxFreq = -1;
     for(int i = 0; i < n; i++) {
-        if(freq[i] != visited && freq[i] > maxfreq) {
-            maxfreq = freq[i];
-            maxelement = arr[i];
+        if(freq[i] != visited && freq[i] > maxFreq) {
+            maxFreq = freq[i];
         }
     }
 
-    printf("%d\n", maxelement);
+    int countMax = 0;
+    int maxElement = -1;
+    for(int i = 0; i < n; i++) {
+        if(freq[i] == maxFreq) {
+            countMax++;
+            maxElement = arr[i]; // overwrite, but will use only if countMax == 1
+        }
+    }
+
+    if(countMax == 1)
+        printf("%d\n", maxElement);
+    else
+        printf("-1\n");
 
     return 0;
 }
